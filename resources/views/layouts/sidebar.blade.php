6a4f0aaee6e0aa0a72f272457b4cbb8076dd6c78
islamdiab-stack <islamdiab967@gmail.com>
2021-03-15 04:19:46 +0200

    Create aside code for demo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,24 @@
-<!-- Sidebar -->
-
-
-
-<!-- ./End sidebar -->
+<div class="sidebar">
+    <nav id="sidebar-menu">
+        <ul class="navbar-nav list-unstyled">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+        </ul>
+    </nav>
+</div>
